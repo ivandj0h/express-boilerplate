f016abc53cc4b2bfc46b3b2ba141081fcda225c3
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-04 04:36:23 +0800

    added controller

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const userController = () => {
+  return (users = {
+    firstName: "juna",
+    lastName: "tirta djoh",
+    age: 6,
+  });
+};
+
+module.exports = userController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+console.log(userController());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  const { firstName, lastName, age } = userController();
+
+  res.render("user", {
+    title: firstName,
+    message: lastName,
+    age: age,
+  });
 });
 
 module.exports = router;
